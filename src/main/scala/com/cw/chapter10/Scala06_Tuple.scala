c1dde30d5a3bfd42a8a3241cb39647ede702c2fa
cw1322311203 <1322311203@qq.com>
2019-12-01 22:28:28 +0800

    add and modify

---
+++ src/main/scala/com/cw/chapter10/Scala06_Tuple.scala
@@ -0,0 +1,36 @@
+package com.cw.chapter10
+
+object Scala06_Tuple {
+  def main(args: Array[String]): Unit = {
+
+    // Tuple:元祖
+    // Map(k -> v)
+    // 将无关的数据当成一个整体来使用
+    // empid,email,ordernum
+    // 使用小括号将数据关联在一起,形成一个整体
+    // 元祖中最多的元素数据的个数为22个
+    val tuple: (String, Int, String) = ("zhangsan", 11111, "xxxxxx")
+
+    // 访问元祖中的数据,调用相应的顺序编号: _顺序号
+    println(tuple._1)
+    println(tuple._2)
+    println(tuple._3)
+
+    // 循环遍历元祖
+    for (elem <- tuple.productIterator) {
+      println(elem)
+    }
+
+    // 如果元祖中元素的个数为2,那么称之为对偶,类似于Map中键值对
+    val tuple1: (Int, String) = (1, "zhangsan")
+    val tupleMap: Map[Int, String] = Map((1,"zhangsan"))
+
+    tupleMap.foreach(t=>{println(t)})
+    tupleMap.foreach(t=>{println(t._1+"="+t._2)})
+  }
+}
+
+class User {
+  var username: String = _
+  var age: Int = _
+}
