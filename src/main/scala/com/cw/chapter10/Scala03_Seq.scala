658cdfa2942335bb2e07f1967633d29270ea35c0
cw1322311203 <1322311203@qq.com>
2019-12-01 21:49:35 +0800

    add and modify

---
+++ src/main/scala/com/cw/chapter10/Scala03_Seq.scala
@@ -0,0 +1,103 @@
+package com.cw.chapter10
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object Scala03_Seq {
+  def main(args: Array[String]): Unit = {
+
+    // TODO 序列 seq
+
+    // TODO 不可变集合
+    // TODO 默认scala提供的集合都是不可变的(immutable)
+    val list: List[Int] = List(1, 2, 3, 4)
+    val list1: List[Int] = List(5, 6, 7, 8)
+
+    // 通过索引获取数据
+    //println(list(0))
+
+    // 增加数据
+
+    // TODO :+ 在集合最后面添加元素   +: 在集合最前面添加元素
+    //val list1: List[Int] = list :+ 1
+    //val list1: List[Int] = 1 +: list // 产生新的集合
+    //println(list1.mkString(","))
+    //println(list == list1) //false
+
+    // TODO ++ 用于连接两个集合
+    //val list2: List[Int] = list.++(list1)
+    //val list2: List[Int] = list ++ list1
+    //println(list2.mkString(","))
+
+    // TODO ::    x::list表示在list前面添加元素x
+    // x::list等价于list.::(x)
+    //val list3: List[Int] = list.::(9)
+    //println(list3.mkString(","))
+    // list中的冒号运算符的运算顺序从右到左
+    val list4: List[Int] = 7 :: 8 :: 9 :: list
+    //val list4: List[Int] = (7 :: (8 :: (9 :: list)))
+
+    // 将集合本身加了进去,并不是把集合元素加进去了
+    // :: 符号::表示向集合中  新建集合添加元素
+    // ::: 运算符是将集合中的每一个元素加入到空集合中去
+    val list5 = 9 :: list1 :: list // 9,List(5, 6, 7, 8),1,2,3,4
+    val list6 = 9 :: list1 ::: list // 9,5,6,7,8,1,2,3,4
+
+    println(list5.mkString(","))
+    println(list6.mkString(","))
+
+
+    // TODO 特殊List集合 : 空集合Nil
+    println(List())
+    println(Nil)
+    println(1 :: 2 :: 3 :: Nil)
+
+
+    // 修改
+    val list7: List[Int] = list.updated(2, 5)
+    println(list7.mkString(","))
+
+    // 删除数据
+
+    val list8: List[Int] = list.drop(2)
+    println(list8.mkString(","))
+
+    // 查询数据
+    for (elem <- list7) {
+      println(elem)
+    }
+
+    // TODO 可变集合ListBuffer
+
+    var mlist = ListBuffer(1, 2, 3, 4)
+    //mlist.insert()
+    //mlist.update()
+    //mlist.drop()
+    //mlist.remove()
+
+    // TODO 集合的属性
+    // 头
+    println(mlist.head) // 1
+    // 尾(除了头以外)
+    println(mlist.tail) // ListBuffer(2, 3, 4)
+    // 最后一个
+    println(mlist.last)
+    // 初始化(除了最后一个)
+    println(mlist.init)
+
+
+    // TODO 队列(一定可变)
+    val q = mutable.Queue(1,2,3,4)
+    println("add before = "+q)
+    q.enqueue(5)
+    println("add after = "+q)
+    val i: Int = q.dequeue() // 队列先进先出
+    println("i="+i)
+    println("delete after = "+q)
+    q.dequeue()
+    q.dequeue()
+    q.dequeue()
+    println("delete after = "+q)
+
+  }
+}
